2af5e59a08a0a5228189f3bd8ab61c5fc96af1b2
FlyHighhher <>
2023-11-20 20:07:50 +0000

    Create function for game board print out and set variables

--- run.py
+++ run.py
@@ -1,5 +1,28 @@
 # Provide functions related to random number generation.
 import random
 
-# Game board creation
-def board_creation
+# Game board creation grid size for user prompt grid size.
+size = int(input("Enter the size of the grid: "))
+
+
+# Function for board creation
+def board_creation(size):
+    return [["0" for _ in range(size)] for _ in range(size)]
+
+
+# Function for game board creation
+def print_board(board):
+    for row in board:
+        print(" ".join(row))
+
+# Variables for board creation
+board_player = board_creation(size)
+board_computer = board_creation(size)
+
+
+# Print out variables for boards
+print("Player's board:")
+print_board(board_player)
+
+print("\nComputer's board:")
+print_board(board_computer)
