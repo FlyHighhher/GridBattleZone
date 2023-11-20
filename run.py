80bcb222d8c1602ce1ef518619da54f71f026ca9
FlyHighhher <>
2023-11-20 19:39:35 +0000

    Create the initial commit

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Provide functions related to random number generation.
+import random
+
+# Game board creation
+def board_creation
