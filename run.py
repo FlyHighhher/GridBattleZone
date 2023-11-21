400eb037f93b13a609d7ad2ec351c716c6b37969
FlyHighhher <>
2023-11-21 11:54:05 +0000

    Add ship placement variables and function for ship placement

--- run.py
+++ run.py
@@ -15,6 +15,7 @@ def print_board(board):
     for row in board:
         print(" ".join(row))
 
+
 # Variables for board creation
 board_player = board_creation(size)
 board_computer = board_creation(size)
@@ -26,3 +27,19 @@ print_board(board_player)
 
 print("\nComputer's board:")
 print_board(board_computer)
+
+
+# Ship placement function on the board
+def place_ships(board, num):
+    for _ in range(num):
+        row = random.randint(0, len(board) - 1)
+        col = random.randint(0, len(board[0]) - 1)
+        while board[row][col] == "X":
+            row = random.randint(0, len(board) - 1)
+            col = random.randint(0, len(board[0]) - 1)
+        board[row][col] = "X"
+
+
+# Ship placement variables
+place_ships(board_player, 5)
+place_ships(board_computer, 5)
